0aaeaf4efc1aae96c59976cc3c4af0ef3f9612c4
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-11-10 21:33:06 +0700

    updated backendgcf ahay

--- admin_test.go
+++ admin_test.go
@@ -1,32 +1,32 @@
-package backendgcf
+// package backendgcf
 
-import (
-	"fmt"
-	"testing"
-	module "github.com/nugisorange/backendgcf/module"
+// import (
+// 	"fmt"
+// 	"testing"
+// 	module "github.com/nugisorange/backendgcf/module"
 
-)
+// )
 
-// user
-func TestInsertUser(t *testing.T) {
-	mconn := SetConnection("MONGOSTRING", "proyek3")
-	var userdata User
-	userdata.Username = "syahid"
-	userdata.Role = "admin"
-	userdata.Password = "kepoah"
+// // user
+// func TestInsertUser(t *testing.T) {
+// 	mconn := SetConnection("MONGOSTRING", "testgis")
+// 	var userdata User
+// 	userdata.Username = "faisal"
+// 	userdata.Role = "admin"
+// 	userdata.Password = "sankuyges"
 
-	nama := InsertUser(mconn, "user", userdata)
-	fmt.Println(nama)
-}
+// 	nama := InsertUser(mconn, "user", userdata)
+// 	fmt.Println(nama)
+// }
 
-func TestGetAllUserFromUsername(t *testing.T) {
-	mconn := SetConnection("MONGOSTRING", "proyek3")
-	anu := module.GetUserFromUsername(mconn, "user", "syahid")
-	fmt.Println(anu)
-}
+// func TestGetAllUserFromUsername(t *testing.T) {
+// 	mconn := SetConnection("MONGOSTRING", "testgis")
+// 	ahay := module.GetUserFromUsername(mconn, "user", "faisal")
+// 	fmt.Println(ahay)
+// }
 
-func TestGetAllUser(t *testing.T) {
-	mconn := SetConnection("MONGOSTRING", "proyek3")
-	anu := module.GetAllUser(mconn, "user")
-	fmt.Println(anu)
-}
+// func TestGetAllUser(t *testing.T) {
+// 	mconn := SetConnection("MONGOSTRING", "testgis")
+// 	ahay := module.GetAllUser(mconn, "user")
+// 	fmt.Println(ahay)
+// }
