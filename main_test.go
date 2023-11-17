58b5f4ba906210db1cd961996a86f5d0666feba8
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-11-17 08:54:59 +0700

    updated

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package backendgcf
+
+import (
+	"fmt"
+	"testing"
+	"github.com/whatsauth/watoken"
+)
+ 
+func TestGeneratePrivateKeyPaseto(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println(privateKey)
+	fmt.Println(publicKey)
+	hasil, err := watoken.Encode("faisaltes", privateKey)
+	fmt.Println(hasil, err)
+}
+
+func TestInsertUser(t *testing.T) {
+	mconn := GetConnectionMongo("MONGOSTRING", "gisaw")
+	var userdata User
+	userdata.Username = "shidiq"
+	userdata.Password = "user123"
+
+	nama := InsertUser(mconn, "user", userdata)
+	fmt.Println(nama)
+}
--- mongo.go
+++ mongo.go
@@ -2,23 +2,70 @@ package backendgcf
 
 import (
 	"os"
+	"context"
 
 	"github.com/aiteung/atdb"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-func SetConnection(MONGOCONNSTRINGENV, dbname string) *mongo.Database {
-	var DBmongoinfo = atdb.DBInfo{
-		// DBString: "mongodb+srv://admin:admin@projectexp.pa7k8.gcp.mongodb.net", //os.Getenv(MONGOCONNSTRINGENV),
-		DBString: os.Getenv(MONGOCONNSTRINGENV),
+func GetConnectionMongo(MongoString, dbname string) *mongo.Database {
+	MongoInfo := atdb.DBInfo{
+		DBString: os.Getenv(MongoString),
 		DBName:   dbname,
 	}
-	return atdb.MongoConnect(DBmongoinfo)
+	conn := atdb.MongoConnect(MongoInfo)
+	return conn
 }
 
 func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata User) bool {
 	filter := bson.M{"username": userdata.Username}
 	res := atdb.GetOneDoc[User](mongoconn, collection, filter)
 	return CheckPasswordHash(userdata.Password, res.Password)
+}
+
+func GetAllData(MongoConnect *mongo.Database, colname string) []GeoJson {
+	data := atdb.GetAllDoc[[]GeoJson](MongoConnect, colname)
+	return data
+}
+
+func InsertDataLonlat(MongoConn *mongo.Database, colname string, coordinate []float64, name, volume, tipe string) (InsertedID interface{}) {
+	req := new(LonLatProperties)
+	req.Type = tipe
+	req.Coordinates = coordinate
+	req.Name = name
+	req.Volume = volume
+
+	ins := atdb.InsertOneDoc(MongoConn, colname, req)
+	return ins
+}
+
+func UpdateDataGeojson(MongoConn *mongo.Database, colname, name, newVolume, newTipe string) error {
+    // Filter berdasarkan nama
+    filter := bson.M{"name": name}
+
+    // Update data yang akan diubah
+    update := bson.M{
+        "$set": bson.M{
+            "volume": newVolume,
+            "tipe":   newTipe,
+        },
+    }
+
+    // Mencoba untuk mengupdate dokumen
+    _, err := MongoConn.Collection(colname).UpdateOne(context.TODO(), filter, update)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func DeleteDataGeojson(MongoConn *mongo.Database, colname string, name string) (*mongo.DeleteResult, error) {
+    filter := bson.M{"name": name}
+    del, err := MongoConn.Collection(colname).DeleteOne(context.TODO(), filter)
+    if err != nil {
+        return nil, err
+    }
+    return del, nil
 }
